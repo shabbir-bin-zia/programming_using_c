56d4a7796c0255db108e6a1584400ae4e007c111
shabbir-bin-zia <shabbir.bubt@gmail.com>
2023-11-03 23:05:33 +0600

    I Have add a hello and data type programm

---
+++ datatype/Datatype.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    //int a;
+    //char b;
+   /* float c;
+    double d;
+    short int e;
+    long int f;
+    long long int g;
+    long double h;*/
+
+
+    //char str[20];
+    //gets(str);
+    //printf("%s", str);
+
+    char str[20];
+   scanf("%[^\n]%*c", str);
+   printf("%s", str);
+
+    //scanf("%d", &a);
+    //scanf("%s", &b);
+    /*scanf("%f", &c);
+    scanf("%lf", &d);
+    scanf("%hd", &e);
+    scanf("%ld", &f);
+    scanf("%lld", &g);
+    scanf("%Lf", &h);*/
+
+    //printf("a = %d\n", a);
+    //printf("b = %c", b);
+    /* printf("%d", c);
+    printf("%d", d);
+    printf("%d", e);
+    printf("%d", f);
+    printf("%d", g);
+    printf("%d", h);*/
+    //puts(b);
+
+    return 0;
+}
